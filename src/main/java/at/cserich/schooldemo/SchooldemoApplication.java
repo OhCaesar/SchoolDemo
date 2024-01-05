260daacde4965afba8998ae74b5cdd80c1e97bd6
OhCaesar <cscode.philipp@gmail.com>
2024-01-05 12:47:57 +0100

    Initial Commit -- SpringBoot Project

---
+++ src/main/java/at/cserich/schooldemo/SchooldemoApplication.java
@@ -0,0 +1,13 @@
+package at.cserich.schooldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/cserich/schooldemo/SchooldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package at.cserich.schooldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
